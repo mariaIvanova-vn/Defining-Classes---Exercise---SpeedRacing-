092262b4abbafc32f7538809d1845c3c8d90e3b7
User <User@DESKTOP-VDOVIB3>
2022-06-03 16:57:44 +0300

    Add project files.

---
+++ DefiningClasses/StartUp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string data = Console.ReadLine();
+                string model = data.Split()[0];
+                double fuelAmount = double.Parse(data.Split()[1]);
+                double fuelConsumption = double.Parse(data.Split()[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumption);
+                cars.Add(car);
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string carModel = command.Split()[1];
+                double amountKm = double.Parse(command.Split()[2]);
+
+                Car carToDrive = cars.First(c => c.Model == carModel);
+                carToDrive.Drive(amountKm);
+            }
+            foreach (Car item in cars)
+            {
+                Console.WriteLine($"{item.Model} {item.FuelAmount:f2} {item.TravelledDistance}");
+            }
+        }
+    }
+}
